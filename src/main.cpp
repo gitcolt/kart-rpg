8f857232d881603cf65ba04070bea45cb257a549
colt <colten@utexas.edu>
2021-03-30 22:42:53 -0500

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,222 @@
+#include "imgui/imgui.h"
+#include "imgui/backends/imgui_impl_glfw.h"
+#include "imgui/backends/imgui_impl_opengl3.h"
+#include "linmath.h"
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+//#include <glm/ext.hpp>
+#include <iostream>
+#include <chrono>
+#include <cmath>
+
+void check_compile_errors(GLuint shader, std::string type);
+
+static const GLchar* vertex_shader_source = R"glsl(
+#version 150 core
+in vec3 pos;
+in vec2 texcoord;
+out vec2 texcoord_out;
+
+uniform mat4 transform;
+void main()
+{
+  gl_Position = transform * vec4(pos, 1.0);
+  texcoord_out = vec2(texcoord.x, texcoord.y);
+}
+)glsl";
+
+static const GLchar* fragment_shader_source = R"glsl(
+#version 150 core
+in vec2 texcoord_out;
+out vec4 out_color;
+uniform sampler2D tex;
+void main()
+{
+  out_color = texture(tex, texcoord_out);
+}
+)glsl";
+
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+  if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
+
+static void error_callback(int error, const char* description) {
+  std::cerr << "Error: " << description << "\n";
+}
+
+static int screen_width = 1280;
+static int screen_height = 720;
+
+int main() {
+  glfwSetErrorCallback(error_callback);
+  if (!glfwInit()) {
+    std::cerr << "Failed to initialize GLFW\n";
+  }
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  auto window = glfwCreateWindow(screen_width, screen_height, "Kart RPG", NULL, NULL);
+  if (!window)
+    std::cerr << "Failed to create window\n";
+
+  glfwSetKeyCallback(window, key_callback);
+  glfwMakeContextCurrent(window);
+  glfwSwapInterval(1);
+  bool err = gladLoadGL() == 0;
+  if (err)
+    std::cerr << "Failed to initialize OpenGL context\n";
+
+  IMGUI_CHECKVERSION();
+  ImGui::CreateContext();
+  ImGuiIO& io = ImGui::GetIO(); (void)io;
+  ImGui_ImplGlfw_InitForOpenGL(window, true);
+  const char* glsl_version = "#version 150 core";
+  ImGui_ImplOpenGL3_Init(glsl_version);
+  ImGui::StyleColorsDark();
+
+  auto t_start = std::chrono::high_resolution_clock::now();
+
+  GLuint vao;
+  glGenVertexArrays(1, &vao);
+  glBindVertexArray(vao);
+
+  GLuint vbo;
+  glGenBuffers(1, &vbo);
+  GLfloat vertices[] = {
+  // x, y, z              u, v
+    -0.5f,  0.5f, 0.0f,   0.0f, 0.0f,
+     0.5f,  0.5f, 0.0f,   1.0f, 0.0f,
+     0.5f, -0.5f, 0.0f,   1.0f, 1.0f,
+    -0.5f, -0.5f, 0.0f,   0.0f, 1.0f
+  };
+  glBindBuffer(GL_ARRAY_BUFFER, vbo);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  GLuint ebo;
+  glGenBuffers(1, &ebo);
+  GLuint elements[] = {
+    0, 1, 2,
+    2, 3, 0
+  };
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(elements), elements, GL_STATIC_DRAW);
+
+  GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vertex_shader, 1, &vertex_shader_source, NULL);
+  glCompileShader(vertex_shader);
+  check_compile_errors(vertex_shader, "VERTEX");
+
+  GLuint fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragment_shader, 1, &fragment_shader_source, NULL);
+  glCompileShader(fragment_shader);
+  check_compile_errors(vertex_shader, "FRAGMENT");
+
+  GLuint shader_program = glCreateProgram();
+  glAttachShader(shader_program, vertex_shader);
+  glAttachShader(shader_program, fragment_shader);
+  glLinkProgram(shader_program);
+  check_compile_errors(shader_program, "PROGRAM");
+  glUseProgram(shader_program);
+
+  GLint pos_attrib = glGetAttribLocation(shader_program, "pos");
+  glEnableVertexAttribArray(pos_attrib);
+  glVertexAttribPointer(pos_attrib, 2, GL_FLOAT, GL_FALSE, 5*sizeof(float), 0);
+
+  GLint tex_attrib = glGetAttribLocation(shader_program, "texcoord");
+  glEnableVertexAttribArray(tex_attrib);
+  glVertexAttribPointer(tex_attrib, 2, GL_FLOAT, GL_FALSE, 5*sizeof(float), (void*)(3*sizeof(float)));
+
+  // load texture
+  GLuint tex;
+  glGenTextures(1, &tex);
+  glBindTexture(GL_TEXTURE_2D, tex);
+
+  int width;
+  int height;
+  int num_color_channels;
+  unsigned char* data = stbi_load("src/assets/course.png", &width, &height, &num_color_channels, 0);
+  if (data) {
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+  } else {
+    std::cout << "Failed to load image.\n";
+    std::cout << stbi_failure_reason();
+  }
+  stbi_image_free(data);
+
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+  
+  glm::mat4 mvp;
+
+  while (!glfwWindowShouldClose(window)) {
+    glfwPollEvents();
+    glClearColor(0.0f, 0.2f, 0.4f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
+    ImGui::NewFrame();
+
+    auto t_now = std::chrono::high_resolution_clock::now();
+    float time = std::chrono::duration_cast<std::chrono::duration<float>>(t_now - t_start).count();
+
+    glm::mat4 transform = glm::mat4(1.0f);
+    transform = glm::translate(transform, glm::vec3(0.0f, -0.1f, -0.3f));
+    transform = glm::rotate(transform, glm::radians(time * 10.f), glm::vec3(0.0f, 1.0f, 0.0f));
+    transform = glm::rotate(transform, glm::radians(90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+    glm::mat4 mat_projection = glm::perspective(glm::radians(45.0f), (float)(screen_width/screen_height), 0.1f, 100.0f);
+    transform = mat_projection * transform;
+
+    GLint transform_loc = glGetUniformLocation(shader_program, "transform");
+    glUniformMatrix4fv(transform_loc, 1, GL_FALSE, glm::value_ptr(transform));
+
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+    ImGui::Text(":)");
+    ImGui::Render();
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+    glfwSwapBuffers(window);
+  }
+
+  ImGui_ImplOpenGL3_Shutdown();
+  ImGui_ImplGlfw_Shutdown();
+  ImGui::DestroyContext();
+
+  glDeleteProgram(shader_program);
+  glDeleteShader(vertex_shader);
+  glDeleteShader(fragment_shader);
+  glDeleteBuffers(1, &vbo);
+  glDeleteVertexArrays(1, &vao);
+  glfwDestroyWindow(window);
+  glfwTerminate();
+}
+
+void check_compile_errors(GLuint shader, std::string type) {
+  GLint success;
+  GLchar info_log[1024];
+  if (type != "PROGRAM") {
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+      glGetShaderInfoLog(shader, 1024, NULL, info_log);
+      std::cout << "ERROR::SHADER_COMPILATION_ERROR of type: " << type << "\n" << info_log << "\n";
+    }
+  } else {
+    glGetProgramiv(shader, GL_LINK_STATUS, &success);
+    if (!success) {
+      glGetShaderInfoLog(shader, 1024, NULL, info_log);
+      std::cout << "ERROR::PROGRAM_LINKING_ERROR of type: " << type << "\n" << info_log << "\n";
+    }
+  }
+}
+
